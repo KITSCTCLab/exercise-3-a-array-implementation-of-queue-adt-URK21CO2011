ff1993291391c42356f0a9b8fe1f7ebea76a65c7
URK21CO2011 <110015075+URK21CO2011@users.noreply.github.com>
2022-09-05 21:14:52 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -125,8 +125,7 @@ solution = Solution(length_of_text)
 for index in range(length_of_text):
  solution.push_character(text[index])
  solution.enqueue_character(text[index])
-
-is_palindrome = True
+ is_palindrome = True
 '''
 pop the top character from stack
 dequeue the first character from queue
